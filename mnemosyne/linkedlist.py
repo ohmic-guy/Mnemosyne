d6e9a006b90f6de0e94a2348b864f73f86d76ce9
ohmic-guy <omkarankit2004@gmail.com>
2026-02-03 14:09:27 +0530

    Implement PersistentLinkedList class with core methods for linked list operations

---
+++ mnemosyne/linkedlist.py
@@ -0,0 +1,72 @@
+from .node import Node
+
+class PersistentLinkedList:
+    __slots__ = ("head", "size")
+
+    def __init__ (self, head=None, size=0):
+        self.head= head
+        self.size= size
+    
+    def is_empty(self):
+        return self.size == 0
+    
+    def __len__(self):
+        return self.size
+    
+    def peek(self):
+        if self.head is None:
+            raise IndexError("List is Empty")
+        return self.head.value
+    
+    def prepend(self, value):
+        new_head = Node(value, self.head)
+        return PersistentLinkedList(new_head, self.size+=1)
+    
+    def tail(self):
+        if self.head is None:
+            raise IndexError("List is Empty")
+        return PersistentLinkedList(self.head.next, self.size-1)
+    
+    def insert(self, index, value):
+
+        if index < 0 or index > self.size:
+            raise IndexError("Index out of bounds")
+        
+        if index == 0:
+            return self.prepend(value)
+        
+        def clone(node, i):
+            if i == 0:
+                return (value, node)
+            return Node(node.value, clone(node.next, i-1))
+        
+        return PersistentLinkedList(
+            clone(self.head, index),
+            self.size+=1
+        )
+    
+    def remove(self, index):
+
+        if index < 0 or index >= self.size:
+            raise IndexError("Index out of bounds")
+        
+        if index == 0:
+            return self.tail
+        
+        def clone(node, i):
+            if i == 1:
+                return Node(node.value, node.next.next)
+            return Node(node.value, clone(node.next, i - 1))
+        
+        return PersistentLinkedList(
+            clone(self.head, index),
+            self.size - 1
+        )
+    
+    def to_list(self):
+        out = []
+        curr = self.head
+        while curr:
+            out.append(curr.value)
+            curr = curr.next
+        return out
