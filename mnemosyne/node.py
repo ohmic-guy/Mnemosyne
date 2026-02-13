ddb27018a413e8daa68fa1d3b5eef43b08250033
ohmic-guy <omkarankit2004@gmail.com>
2026-02-13 15:43:19 +0530

    changed the node structure with SinglyNode and DoublyNode for Single Linked and Double Linked Persistent Data Structures

--- mnemosyne/node.py
+++ mnemosyne/node.py
@@ -1,8 +1,32 @@
 # mnemosyne/node.py
 
-class Node:
-    __slots__ = ("value", "prev")
+class SinglyNode:
+    """
+    Immutable node for singly linked persistent structures.
+    Used by:
+    - PersistentStack
+    - PersistentQueue (two-stack model)
+    - PersistentLinkedList (singly)
+    """
 
-    def __init__(self, value, prev):
+    __slots__ = ("value", "next")
+
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+
+class DoublyNode:
+    """
+    Immutable node for persistent doubly linked structures.
+    Used by:
+    - PersistentDoublyLinkedList
+    - PersistentDeque (future)
+    """
+
+    __slots__ = ("value", "prev", "next")
+
+    def __init__(self, value, prev=None, next=None):
         self.value = value
         self.prev = prev
+        self.next = next
